27384f61741c1c5957e2b6ee0c91644bf9405898
AntiElysium <patrick@pilsby.dk>
2022-12-13 22:01:51 +0100

    Removed unused project

--- Project/Backend/src/Config/DBSetup.ts
+++
@@ -1,4 +0,0 @@
-export const getDbConnectionString = () => {
-    return `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_DB_NAME}.egjcxgs.mongodb.net/?retryWrites=true&w=majority`
-    //return `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASSWORD}@${process.env.MONGO_ATLAS_DB_NAME}.wk1qr.mongodb.net/?retryWrites=true&w=majority`
-}
--- Project/Frontend/src/common/Constants.ts
+++
@@ -1,5 +0,0 @@
-const constansts = {
-    serverBaseURL: (process.env.NODE_ENV === 'production') ? 'https://iob.news:3000' : 'http://localhost:3000' //TODO: If deploying to heroku, change URL
-}
-
-export default constansts;
